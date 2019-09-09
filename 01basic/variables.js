8d1b6789c46a5c8f0960cf6f1e2803c0ef16e5c0
Amarmandal <amarmandal2073@gmail.com>
2019-09-09 21:14:55 +0545

    Basic roadmap

---
+++ 01basic/variables.js
@@ -0,0 +1,7 @@
+let firstName = "Amar";
+firstName = "Ammu"; // once declared can be reassigned
+const lastName = " Mandal"; //once declared cannot be reassigned
+// lastName = "Anything"; // will give assignment erro
+let fullName = firstName + lastName;
+console.log(fullName);
+
